9de697fde46c062c85be032bf2566187785e2dc2
arifulmist <arifulm926@gmail.com>
2025-08-03 02:46:40 +0600

    run front page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ff, setff] = useState("Are You Ready ?");
+
+  const abc = () => {
+    setff("LETS GOO!!");
+  };
 
   return (
-    
-      <div>
-       
-      <p className="read-the-docs">
-        Hi guys Lets start the project
-      </p>
+    <div className="app-container">
+      <h1 className="heading">{ff}</h1>
+      <button className="start-button" onClick={abc}>
+        YES
+      </button>
+      <p className="description">Hi guys, let's start the project!</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
