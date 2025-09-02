23b1b95a32e30410610da82bb1e7650c6ffec054
Than Than Thay <202314098@student.mist.ac.bd>
2025-09-03 01:53:00 +0600

    Added filtering script of shelters by area

--- Backend/src/db_scripts/filtering_events.sql
+++ Backend/src/db_scripts/filtering_events.sql
@@ -54,3 +54,22 @@ BEGIN
 END$$
 DELIMITER ;
 
+DELIMITER $$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetSheltersByArea`(IN p_area VARCHAR(50))
+BEGIN
+    SELECT s.Shelter_id,
+           s.Shelter_name,
+           s.Total_capacity,
+           s.Current_capacity,
+           s.Shelter_image,
+           a.Area_name
+    FROM Shelter s
+    JOIN shelter_in_area sa ON s.Shelter_id = sa.shelter_id
+    JOIN Area a ON sa.area_id = a.Area_id
+    WHERE a.Area_name = p_area
+    ORDER BY s.Shelter_name;
+END 
+
+DELIMITER ;
+
